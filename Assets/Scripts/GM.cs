e559db75ee94f7ade5660c1b195d72d12d935508
Tais Souza <ttisas25@gmail.com>
2017-11-21 11:24:59 -0200

    Implementando Timer

--- Assets/Scripts/GM.cs
+++ Assets/Scripts/GM.cs
@@ -10,6 +10,9 @@ public class GM : MonoBehaviour {
 	public float timeToRespawn = 2f;
 	public Transform spawPoint;
 	public GameObject playerPrefab;
+	public float maxTime = 120f;
+	bool timerOn = true;
+	float timeLeft;
 	public UI ui;
 	GameData data = new GameData();
 
@@ -23,6 +26,7 @@ public class GM : MonoBehaviour {
 		if (player == null){
 			RespawnPlayer();
 		}
+		timeLeft = maxTime;
 	}
 	
 
@@ -33,19 +37,27 @@ public class GM : MonoBehaviour {
 				player =obj.GetComponent<PlayerCtrl>();
 			}
 		}
+		UpdateTimer();
 		DisplayHudData();
+	}
+
+	void UpdateTimer(){
+		if (timerOn){
+			timeLeft = timeLeft - Time.deltaTime;
+			if (timeLeft <= 0f) {
+				timeLeft = 0;
+				ExpirePlayer();
+			}
+		}
 	}
 
 	void DisplayHudData(){
 		ui.hud.txtCoinCount.text = "x " + data.coinCount;
+		ui.hud.txtTimer.text = "Timer: " + timeLeft.ToString("F1");
 	}
 
 	public void IncrementCoinCount(){
 		data.coinCount++;
-	}
-
-	public void RespawnPlayer(){
-		Instantiate(playerPrefab, spawPoint.position, spawPoint.rotation);
 	}
 
 	public void KillPlayer(){
@@ -53,6 +65,24 @@ public class GM : MonoBehaviour {
 			Destroy(player.gameObject);
 			Invoke("RespawnPlayer", timeToRespawn);
 		}
+	}
+
+	public void ExpirePlayer(){
+		if (player != null){
+			Destroy(player.gameObject);
+		}
+		GameOver();
+	}
+
+	void GameOver(){
+		timerOn = false;
+		ui.gameOver.txtCoinCount.text = "Coins: " + data.coinCount;
+		ui.gameOver.txtTimer.text = "Timer: " + timeLeft.ToString("F1");
+		ui.gameOver.gameOverPanel.SetActive(true);
+	}
+
+	public void RespawnPlayer(){
+		Instantiate(playerPrefab, spawPoint.position, spawPoint.rotation);
 	}
 	
 }
